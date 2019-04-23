4c9beb5fc5310928e3e9b10a1487fc96a3500c86
Ben Rothermel <benrothermel@hotmail.co.uk>
2019-04-23 14:55:11 +0100

    Added Doxygen

---
+++ BorderArc.Redis/.ci/setup-ssh.sh
@@ -0,0 +1,14 @@
+﻿
+#!/bin/bash
+# setup-ssh.sh: load the SSH key 
+
+set -ev
+declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
+# Decrypt the file containing the private key (put the real name of the variables)
+openssl aes-256-cbc -K $encrypted_48c11f54da3c_key -iv $encrypted_48c11f54da3c_iv -in private.pem.enc -out "$SSH_FILE" -d
+# Enable SSH authentication
+chmod 600 "$SSH_FILE" \
+  && printf "%s\n" \
+       "Host github.com" \
+       "  IdentityFile $SSH_FILE" \
+       "  LogLevel ERROR" >> ~/.ssh/config
