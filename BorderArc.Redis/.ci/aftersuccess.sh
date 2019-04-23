e228f5e0a716c4c4e2366cd9915e655493adec5a
Ben Rothermel <benrothermel@hotmail.co.uk>
2019-04-23 16:03:22 +0100

    added after success to new file

---
+++ BorderArc.Redis/.ci/aftersuccess.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/sh
+# Generate the docs only if master, the travis_build_docs is true and we can use secure variables
+  #if [[ "$TRAVIS_BRANCH" = "master" && -n "$TRAVIS_BUILD_DOCS" && "$TRAVIS_PULL_REQUEST" = "false" ]] ; then
+  #    cd $PROJECT_DIR_ABS
+  #    source .ci/setup-ssh.sh || travis_terminate 1
+  #    .ci/generateDocumentation.sh || travis_terminate 1
+  #  fi
+$TRAVIS_BRANCH = $1
+$TRAVIS_BUILD_DOCS = $2
+$PROJECT_DIR_ABS = $3
+echo "args [$TRAVIS_BRANCH" = "master" && -n "$TRAVIS_BUILD_DOCS]";
+if [[ "$TRAVIS_BRANCH" = "master" && -n "$TRAVIS_BUILD_DOCS" ]] ; then
+    cd $PROJECT_DIR_ABS
+    source .ci/setup-ssh.sh || travis_terminate 1
+    .ci/generateDocumentation.sh || travis_terminate 1
+fi
